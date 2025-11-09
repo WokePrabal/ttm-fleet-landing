a6ffa0094c6e1fc9662617ce9e7132d7c14acd67
WokePrabal <prabalpratap465@gmail.com>
2025-11-09 18:44:07 +0530

    Remove duplicate files differing only by case (cleanup)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Helmet } from "react-helmet";
-import Navbar from "./components/navbar.jsx";
-import Hero from "./components/hero.jsx";
-import FeatureStrip from "./components/featurestrip.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import FeatureStrip from "./components/FeatureStrip.jsx";
 import Benefits from "./components/Benefits.jsx";
 import Integration from "./components/Integration.jsx";
 import SocialProof from "./components/SocialProof.jsx";
 import ContactForm from "./components/ContactForm.jsx";
-import Footer from "./components/footer.jsx";
+import Footer from "./components/Footer.jsx";
+
 
 export default function App() {
   return (
